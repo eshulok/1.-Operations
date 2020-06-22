e4637f42ef3c66b50e0e534f1afb1ea7fd3254de
Liz S <shulok@berkeley.edu>
2020-06-22 16:26:09 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,28 @@
+#This is a comment in Python
+#Comments are for humans
+#Comments are ignored when you run your code
+
+#You can do math operations with Python
+#Addition
+print(5 + 2)
+
+#Subraction
+print(10 - 8)
+
+#Mulitiplication
+print(5 * 10)
+
+#Division
+print(10 / 2)
+
+#The order of operations is important 
+#Mulitiplication and division are higher order operations
+#That means those operations are done first no matter what order they are written
+
+#This will print 60 because multiplication comes first
+print(10 + 5 * 10)
+
+#Use parenthesis to change the default order of operations
+#Operations inside the parenthesis are calculated first
+#This will print 150
+print((10 + 5) * 10)
